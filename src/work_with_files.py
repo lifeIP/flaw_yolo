5ebcb872f76be352362f259e4d07c390b1948dd8
lifeIP <molokeevartem@gmail.com>
2025-02-07 14:19:10 +0700

    Переработана структура проекта

--- src/work_with_files.py
+++ src/work_with_files.py
@@ -6,7 +6,7 @@ def load_settings_from_file(id: int):
     Загружает данные из файлов в переменные: crop_left, crop_right, crop_up, crop_down, confidence_threshold.
     Параметр id отвечает за то, для какого детектора грузятся данные [0,1,2,3].
     '''
-    from manage_service_data import get_service_data_from_file
+    from src.manage_service_data import get_service_data_from_file
     return  int(get_service_data_from_file("crop_left", id)),\
             int(get_service_data_from_file("crop_right", id)),\
             int(get_service_data_from_file("crop_up", id)),\
@@ -20,7 +20,7 @@ def save_settings_into_file(crop_left, crop_right, crop_up, crop_down, confidenc
     Сохраняет данные в файлы из переменных.
     Параметр id отвечает за то, для какого детектора грузятся данные [0,1,2,3].
     '''        
-    from manage_service_data import set_service_data_into_file
+    from src.manage_service_data import set_service_data_into_file
     set_service_data_into_file("crop_left", crop_left, id)
     set_service_data_into_file("crop_right", crop_right, id)
     set_service_data_into_file("crop_up", crop_up, id)
